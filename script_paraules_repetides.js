e2270f8c3bd5c73149673ed7f4d3dc8d97f468de
Jokediaz <jokediaz@gmail.com>
2019-12-17 09:52:35 +0100

    second commit

---
+++ script_paraules_repetides.js
@@ -0,0 +1,45 @@
+// 1er ejercicio
+
+var nom = ['L','u','i','s'];
+      for (i=0;i<nom.length;i ++) {
+    console.log(nom[i]);
+  }
+
+//2on ejercicio
+
+var nom = ['A','L','3','J','A','N','D','R','0'];
+console.log("Partim del nom "+nom);
+for (i=0;i<nom.length;i++) {
+    if(isNaN(nom[i])) {
+       if(/[aeiouAEIOU]/.test(nom[i])){
+         console.log("HE TROBAT LA VOCAL "+nom[i]);
+       }else console.log("HE TROBAT LA CONSONANT "+nom[i]);
+    }else console.log(" Els noms de persones no contenen el número " +nom[i]);
+}
+
+//3er ejercicio
+
+
+//var nom = ['A','L','3','J','A','N','D','R','0']; PUEDES PROBAR CON ESTE ARRAY TB
+var nom = prompt("Introduce la palabra para poder contar");
+var contar= 0;
+var verif;
+for (i=0;i<nom.length;i ++) {
+  verif= nom[i];
+   for (j=0;j<nom.length;j++){
+     if (nom[j] == verif){
+       contar++;
+     }
+   }
+   console.log(nom[i]+ ":" +contar);
+   contar = 0;
+}
+
+//4o ejercicio
+
+var name1 = ["L","u",'i','s'];
+var surname = ['S','a','u','r','a'];
+var fullname = [].concat(name1," ",surname);
+console.log(fullname);
+
+
